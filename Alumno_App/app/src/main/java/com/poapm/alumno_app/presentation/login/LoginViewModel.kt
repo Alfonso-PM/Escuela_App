2e9a7555eda8639c80880e5a3b5bbd2a51836375
Alfonso Peña <poncho_mendoza8a@hotmail.com>
2021-12-06 13:28:12 -0600

    Nav
    de 3 items de prueba a 2 originales en el nav

--- Alumno_App/app/src/main/java/com/poapm/alumno_app/presentation/login/LoginViewModel.kt
+++ Alumno_App/app/src/main/java/com/poapm/alumno_app/presentation/login/LoginViewModel.kt
@@ -1,27 +1,5 @@
 package com.poapm.alumno_app.presentation.login
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.poapm.alumno_app.core.presentation.BaseViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.DelicateCoroutinesApi
-import javax.inject.Inject
 
-@DelicateCoroutinesApi
-@HiltViewModel
-class LoginViewModel @Inject constructor(private val myUser: MyUser):BaseViewModel() {
-
-    val ussername= MutableLiveData("")
-    val password= MutableLiveData("")
-
-    fun Log(){
-    myUser(Alumno(name=ussername.value?:"",password=password.value?:"")){
-        it.fold(::handleFailure){
-            state.value=LoginViewState.UserFound(it.alumno)
-
-
-        true
-            }
-       }
-    }
+class LoginViewModel{
 }
--- Alumno_App/app/src/main/java/com/poapm/alumno_app/presentation/login/LoginViewState.kt
+++ Alumno_App/app/src/main/java/com/poapm/alumno_app/presentation/login/LoginViewState.kt
@@ -2,8 +2,6 @@ package com.poapm.alumno_app.presentation.login
 
 import com.poapm.alumno_app.core.presentation.BaseViewState
 
-class LoginViewState  : BaseViewState() {
-
-    data class UserFound(val alumno: Alumno) : BaseViewState()
+class LoginViewState {
 
 }
